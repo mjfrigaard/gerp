6aa8c5a4950b4331eea087e93a3f6443540e0809
“Martin <mjfrigaard@gmail.com>
2022-09-22 01:11:58 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# `geR2`: Good Enuff R (version 2!)
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of `geR2` is to …
+
+## Installation
+
+You can install the development version of `geR2` like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(geR2)
+## basic example code
+```
---
+++ data-raw/wu_dt.R
@@ -0,0 +1,21 @@
+## code to prepare `wu_dt` dataset goes here
+require(data.table)
+wu_dt <- data.table::data.table(
+      member = c("RZA","GZA","ODB","Method Man",
+                 "Raekwon","Ghostface Killah","Inspectah Deck","U-God","Masta Killa",
+                 "Cappadonna"),
+        name = c("Robert Fitzgerald Diggs",
+                 "Gary Eldridge Gric","Russell Tyrone Jones","Clifford Smith, Jr.",
+                 "Corey Woods","Dennis Coles","Jason Richard Hunter",
+                 "Lamont Jody Hawkins","Jamel Irief","Darryl Hill"),
+        born = c(1969L,1966L,1968L,1971L,1970L,1970L,
+                 1970L,1970L,1969L,1968L),
+        city = c("New York City","New York City",
+                 "New York City","Hempstead, Long Island","New York City",
+                 "Staten Island, New York City","New York City","New York City",
+                 "New York City","New York City"),
+       alive = c(TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)
+)
+usethis::use_data(wu_dt, overwrite = TRUE)
+
+
